32f3be8bca08aade169669a23b929306e1b35bf7
Islam Kadyrov <islam.kadyrov.dlschool@gmail.com>
2021-03-13 17:12:33 +0300

    Добавлен пакет CSVhelper

--- Excel/MainWindow.xaml.cs
+++ Excel/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CsvHelper;
 
 namespace Excel
 {
@@ -23,6 +24,7 @@ namespace Excel
         public MainWindow()
         {
             InitializeComponent();
+            DataGrid dg = new DataGrid();
         }
     }
 }
