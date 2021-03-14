5806d3f11d3940c3b4e8b0448fe17652a99f7005
Islam Kadyrov <islam.kadyrov.dlschool@gmail.com>
2021-03-14 13:44:53 +0300

    Добавлено открытие CSV файлов(кроме coursea, значения столбца race/eth пропали)

--- Excel/MainWindow.xaml.cs
+++ Excel/MainWindow.xaml.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Data.OleDb;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using CsvHelper;
+using Microsoft.Win32;
+using System.Data;
 
 namespace Excel
 {
@@ -24,7 +20,52 @@ namespace Excel
         public MainWindow()
         {
             InitializeComponent();
-            DataGrid dg = new DataGrid();
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "csv файл (*.csv)|*.csv";
+            openFileDialog.Title = "Открыть CSV файл";
+            string path = string.Empty;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                        path = openFileDialog.FileName;
+                }
+
+                catch (Exception)
+                {
+                    MessageBox.Show("Could not load.");
+                    return;
+                }
+            }
+
+            DataTable dt = GetDataTableFromCSV(openFileDialog.FileName);
+            if (dt != null)
+            {
+                dataGrid.ItemsSource = dt.DefaultView;
+            }
+        }
+
+        private DataTable GetDataTableFromCSV(string path)
+        {
+            try
+            {
+                OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + System.IO.Path.GetDirectoryName(path) + ";Extended Properties=\"Text;HDR == YES;FMT = Delimited\"");
+                conn.Open();
+                string strQuery = "Select * from [" + System.IO.Path.GetFileName(path) + "]";
+                OleDbDataAdapter da = new OleDbDataAdapter(strQuery, conn);
+                DataSet ds = new System.Data.DataSet();
+                da.Fill(ds);
+                return ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
