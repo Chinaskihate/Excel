fe955d3f9ca01461cc87b74a578ea8017ff795b0
Islam Kadyrov <islam.kadyrov.dlschool@gmail.com>
2021-03-14 16:39:32 +0300

    Эксперименты с получением данных. Оказалось исправление бага файле с оценками сломало название столбцов в других файлах.

--- Excel/MainWindow.xaml.cs
+++ Excel/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
 using System.Windows;
 using System.Data.OleDb;
 using System.Windows.Controls;
@@ -35,7 +36,7 @@ namespace Excel
             {
                 try
                 {
-                        path = openFileDialog.FileName;
+                    path = openFileDialog.FileName;
                 }
 
                 catch (Exception)
@@ -48,31 +49,62 @@ namespace Excel
             dt = GetDataTableFromCSV(openFileDialog.FileName);
             if (dt != null)
             {
-
                 dataGrid.ItemsSource = dt.DefaultView;
             }
+            else
+            {
+                return;
+            }
+
+            string mess = string.Empty;
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                mess += dt.Rows[0][i];
+                double sum = 0;
+                for (int j = 1; j < dt.Rows.Count; j++)
+                {
+                    try
+                    {
+                        object ob = dt.Rows[j][i];
+                        sum += Convert.ToDouble(dt.Rows[j][i]);
+                    }
+                    catch (Exception)
+                    {
+                        break;
+                    }
+                }
+                mess += sum + " " + Environment.NewLine;
+            }
+
+            MessageBox.Show(mess);
         }
 
         private DataTable GetDataTableFromCSV(string path)
         {
             try
             {
-                OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + System.IO.Path.GetDirectoryName(path) +
-                     ";Extended Properties=\"Text;HDR=NO;IMEX=1\"");
-                conn.Open();
-                string strQuery = "Select * from [" + System.IO.Path.GetFileName(path) + "]";
-                OleDbDataAdapter da = new OleDbDataAdapter(strQuery, conn);
-                DataSet ds = new System.Data.DataSet();
-                da.Fill(ds);
-                return ds.Tables[0];
+                string foo = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + System.IO.Path.GetDirectoryName(path) +
+                     ";Extended Properties=\"Text;HDR=NO;IMEX=1\"";
+                using (OleDbConnection conn = new OleDbConnection(foo))
+                {
+                    conn.Open();
+                    string strQuery = "Select * from [" + System.IO.Path.GetFileName(path) + "]";
+                    OleDbDataAdapter da = new OleDbDataAdapter(strQuery, conn);
+                    DataSet ds = new System.Data.DataSet();
+                    da.Fill(ds);
+                    return ds.Tables[0];
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message,"Ошибка");
+                MessageBox.Show(ex.Message, "Ошибка");
                 return null;
             }
         }
 
+        private void statisticButton_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
     }
 }
