7b6448771a5f7eaec332e0bbf9af61b871227840
Islam Kadyrov <islam.kadyrov.dlschool@gmail.com>
2021-03-15 17:51:08 +0300

    Окно с гистограммами рабочее(нет пункта 5,6,7,8).

--- Excel/HistogramWindow.xaml.cs
+++ Excel/HistogramWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -25,8 +27,85 @@ namespace Excel
         {
             InitializeComponent();
             this.dt = dt;
+            FillBoxData();
         }
 
-        public Func<double,string> Formatter { get; set; }
+        private void FillBoxData()
+        {
+            List<string> names = new List<string>();
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                names.Add(dt.Columns[i].ColumnName);
+            }
+            boxData.ItemsSource = names;
+        }
+
+        private void FillHistogram(string columnName)
+        {
+            BarCollection = GetSeriesCollection(columnName);
+
+            BarLabels = GetBarLabels(columnName);
+
+            Formatter = value => value.ToString("N");
+            DataContext = this;
+            chart.AxisX[0].Labels = BarLabels;
+            chart.Series = BarCollection;
+        }
+
+        private SeriesCollection GetSeriesCollection(string columnName)
+        {
+            SeriesCollection series = new SeriesCollection();
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string key = dt.Rows[i][columnName].ToString();
+                if (dict.ContainsKey(key))
+                {
+                    dict[key]++;
+                }
+                else
+                {
+                    dict.Add(key, 1);
+                }
+            }
+            ChartValues<double> values = new ChartValues<double>();
+
+            foreach (var key in dict.Keys)
+            {
+                values.Add(dict[key]);
+            }
+
+            series.Add(new ColumnSeries
+            {
+                Title = columnName,
+                Values = values
+            });
+
+            return series;
+        }
+
+        private List<string> GetBarLabels(string columnName)
+        {
+            List<string> barLabels = new List<string>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string barLabel = dt.Rows[i][columnName].ToString();
+                if (!barLabels.Contains(barLabel))
+                {
+                    barLabels.Add(barLabel);
+                }
+            }
+            return barLabels;
+        }
+
+        public Func<double, string> Formatter { get; set; }
+        public SeriesCollection BarCollection { get; set; }
+        public List<string> BarLabels { get; set; }
+
+        private void boxData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FillHistogram(boxData.SelectedItem.ToString());
+        }
     }
+
 }
